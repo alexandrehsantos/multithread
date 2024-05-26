96f884c55ce6289c6fb2b96cbe74206589058547
Alex <alexandre@fns.llc>
2024-05-26 19:05:39 +0400

    Reentranlock

---
+++ practice/Reenterantlock/Main.java
@@ -0,0 +1,13 @@
+public class Main extends Application {
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("Cryptocurrency Prices");
+
+        GridePane grid = createGride();
+        
+    }
+}
